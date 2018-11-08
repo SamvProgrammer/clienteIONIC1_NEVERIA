4a07a9ab6ecce0080dcb9aa6761e766286fa5432
Samv <santiagoantoniomariscal@gmail.com>
2018-11-08 09:28:13 -0600

    iniciando cliente nevería

---
+++ www/js/controllers.js
@@ -0,0 +1,50 @@
+angular.module('app.controllers', [])
+  
+.controller('menuCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('menDeOpcionesCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('pedidosHechosCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('historialCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+      
+.controller('menDeProductosCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('descripciNDelProductoCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+ 
---
+++ www/js/routes.js
@@ -0,0 +1,71 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+
+      .state('tabsController.menDeOpciones', {
+    url: '/page2',
+    views: {
+      'tab1': {
+        templateUrl: 'templates/menDeOpciones.html',
+        controller: 'menDeOpcionesCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.pedidosHechos', {
+    url: '/page3',
+    views: {
+      'tab2': {
+        templateUrl: 'templates/pedidosHechos.html',
+        controller: 'pedidosHechosCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.historial', {
+    url: '/page4',
+    views: {
+      'tab3': {
+        templateUrl: 'templates/historial.html',
+        controller: 'historialCtrl'
+      }
+    }
+  })
+
+  .state('tabsController', {
+    url: '/page1',
+    templateUrl: 'templates/tabsController.html',
+    abstract:true
+  })
+
+  .state('tabsController.menDeProductos', {
+    url: '/productos',
+    views: {
+      'tab1': {
+        templateUrl: 'templates/menDeProductos.html',
+        controller: 'menDeProductosCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.descripciNDelProducto', {
+    url: '/descripcion',
+    views: {
+      'tab1': {
+        templateUrl: 'templates/descripciNDelProducto.html',
+        controller: 'descripciNDelProductoCtrl'
+      }
+    }
+  })
+
+$urlRouterProvider.otherwise('/page1/page2')
+
+
+});
